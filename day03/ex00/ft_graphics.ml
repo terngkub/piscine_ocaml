f4be5c9686c0a23bc66dd6871a1709f28f2a16d5
terngkub <terngnattapol@gmail.com>
2019-05-22 18:50:20 +0200

    finish day03/ex00

--- day03/ex00/ft_graphics.ml
+++ day03/ex00/ft_graphics.ml
@@ -10,41 +10,38 @@ let draw_square x y size =
   Graphics.lineto (x + padding) (y + padding)
 
 
-let draw_rect x y size_x size_y =
-  let padding_x = size_x / 2 in
-  let padding_y = size_y / 2 in
+let draw_rect_text x y str =
+  let padding_x = 30 in
+  let padding_y = 15 in
   Graphics.moveto (x + padding_x) (y + padding_y);
   Graphics.lineto (x + padding_x) (y - padding_y);
   Graphics.lineto (x - padding_x) (y - padding_y);
   Graphics.lineto (x - padding_x) (y + padding_y);
-  Graphics.lineto (x + padding_x) (y + padding_y)
-
-
-let draw_rect_text x y str =
-  draw_rect x y 60 30;
+  Graphics.lineto (x + padding_x) (y + padding_y);
   Graphics.moveto (x - 15) (y - 5);
   Graphics.draw_string str
 
 
 let draw_tree_node node =
-  match node with
-  | Node (v, Nil, Nil) ->
+  Graphics.open_graph " 400x300";
   begin
-    draw_rect_text 150 150 v;
-    Graphics.moveto 180 150;
-    Graphics.lineto 220 100;
-    draw_rect_text 250 100 "Nil";
-    Graphics.moveto 180 150;
-    Graphics.lineto 220 200;
-    draw_rect_text 250 200 "Nil";
-    ()
-  end
-  | Nil | _ -> draw_rect_text 200 150 "Nil"
+    match node with
+    | Node (v, Nil, Nil) ->
+    begin
+      draw_rect_text 150 150 v;
+      Graphics.moveto 180 150;
+      Graphics.lineto 220 100;
+      draw_rect_text 250 100 "Nil";
+      Graphics.moveto 180 150;
+      Graphics.lineto 220 200;
+      draw_rect_text 250 200 "Nil"
+    end
+    | Nil | _ -> draw_rect_text 200 150 "Nil"
+  end;
+  ignore (Graphics.read_key ())
 
 
 let main () =
-  Graphics.open_graph " 400x300";
-  draw_tree_node (Node ("hello", Nil, Nil));
-  ignore (Graphics.read_key ())
+  draw_tree_node (Node ("hello", Nil, Nil))
 
-let _ = main ()
+let () = main ()
